c9a119412c6f199c2617a16d295cfcc6bafe8b04
Théo SABATTIE (ilicos) <fondateur.ilicos@gmail.com>
2016-05-12 22:21:50 +0200

    Added tests on Main

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -1,7 +1,11 @@
 package template;
 
+import openfl.Assets;
+import openfl.display.StageScaleMode;
 import openfl.events.Event;
 import template.game.Game;
+import template.utils.game.GameStage;
+import template.utils.game.GameStageAlign;
 import template.utils.metadata.Metadatas;
 import openfl.display.Sprite;
 
@@ -14,7 +18,14 @@ class Main extends Sprite
 
 	public function new() {
 		super();
-
+		
+		var gameStage:GameStage = new GameStage();
+		addChild(gameStage);
+		gameStage.addGameChild(Assets.getMovieClip("TitleCard:TitleCard"));
+		gameStage.alignMode = GameStageAlign.CENTER;
+		gameStage.scaleMode = StageScaleMode.SHOW_ALL;
+		gameStage.centerGameContainer = true;
+		
 		addEventListener(Event.ENTER_FRAME, Game.gameloop);
 		Metadatas.load();
 		Game.start();
