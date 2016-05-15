6b4f2cae8d0c3b7db751b799c6ccc3c7569b09d3
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-05-15 17:06:40 +0200

    Add GameStage test.

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -1,13 +1,17 @@
 package template;
 
+import haxe.Timer;
 import openfl.Assets;
 import openfl.display.Sprite;
 import openfl.display.StageScaleMode;
 import openfl.events.Event;
+import openfl.geom.Point;
 import template.game.Game;
+import template.utils.game.ResizeComp;
 import template.utils.game.GameStage;
 import template.utils.game.GameStageAlign;
 import template.utils.metadata.Metadatas;
+import template.utils.debug.GraphicPoint;
 
 /**
  * ...
@@ -18,15 +22,29 @@ class Main extends Sprite
 	public function new() {
 		super();
 		
-		var gameStage:GameStage = new GameStage();
-		addChild(gameStage);
-		gameStage.addScreen(Assets.getMovieClip("TitleCard:TitleCard"));
-		gameStage.alignMode = GameStageAlign.CENTER;
-		gameStage.scaleMode = StageScaleMode.SHOW_ALL;
-		gameStage.gameContainerAlignMode = GameStageAlign.CENTER;
-		
 		addEventListener(Event.ENTER_FRAME, Game.gameloop);
 		Metadatas.load();
 		Game.start();
+		
+		var sprite2:Sprite = new Sprite();
+		var sprite3:Sprite = new Sprite();
+		sprite2.x += 200;
+		sprite2.y += 50;
+		
+		sprite2.scaleX = sprite2.scaleY = 2;
+		sprite3.scaleX = sprite3.scaleY = 2;
+		
+		var sprite:Sprite = Assets.getMovieClip("TitleCard:TitleCard");
+		
+		var comp:ResizeComp 	= new ResizeComp(sprite);
+		comp.scaleMode 		    = ScaleMode.SHOW_ALL;
+		comp.useSafeZone        = true;
+		comp.safeZoneAlignMode  = AlignMode.CENTER;
+		comp.alignModeOnResize  = AlignMode.CENTER;
+		
+		sprite3.addChild(sprite2);
+		
+		addChild(sprite3);
+		sprite2.addChild(sprite);
 	}
 }
