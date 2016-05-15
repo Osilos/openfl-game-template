8c260c8244eb89cd998b10e568861e768e5f193c
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-05-15 17:39:42 +0200

    Organize import.

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -1,12 +1,10 @@
 package template;
 
 import openfl.display.Sprite;
-import template.utils.debug.DebugInfo;
-import template.utils.debug.Debug;
 import openfl.events.Event;
 import template.game.Game;
+import template.utils.debug.Debug;
 import template.utils.debug.DebugInfo;
-import template.utils.localization.Localization;
 import template.utils.metadata.Metadatas;
 
 /**
@@ -16,13 +14,12 @@ import template.utils.metadata.Metadatas;
 class Main extends Sprite {
 	public function new() {
 		super();
-
 		Debug.initDefaultContainer(this); //todo : create debug container
 		addEventListener(Event.ENTER_FRAME, Game.gameloop);
 		
 		Metadatas.load();
 		Game.start();
-
+		
 		// todo : move to Game.hx when we have GameStage
 		#if showdebuginfo
 			var debugInfo:DebugInfo = new DebugInfo();
