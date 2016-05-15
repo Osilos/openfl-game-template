f9da00ddace44e2b9ebe461de6345c3e8bc36d29
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-05-15 20:38:19 +0200

    Remove tests.

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -24,27 +24,6 @@ class Main extends Sprite {
 		Metadatas.load();
 		Game.start();
 		
-		var sprite2:Sprite = new Sprite();
-		var sprite3:Sprite = new Sprite();
-		sprite2.x += 200;
-		sprite2.y += 50;
-		
-		sprite2.scaleX = sprite2.scaleY = 2;
-		sprite3.scaleX = sprite3.scaleY = 2;
-		
-		var sprite:Sprite = Assets.getMovieClip("TitleCard:TitleCard");
-		
-		var comp:ResizeComp 	= new ResizeComp(sprite);
-		comp.scaleMode 		    = ScaleMode.FIT_ALL;
-		comp.useSafeZone        = true;
-		comp.safeZoneAlignMode  = AlignMode.CENTER;
-		comp.alignModeOnResize  = AlignMode.CENTER;
-		
-		sprite3.addChild(sprite2);
-		
-		addChild(sprite3);
-		sprite2.addChild(sprite);
-		// todo : move to Game.hx when we have GameStage
 		#if showdebuginfo
 			var debugInfo:DebugInfo = new DebugInfo();
 			addChild(debugInfo);
