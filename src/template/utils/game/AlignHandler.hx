c34b20a20cffcfec02505a7ce5e17860e619c310
Théo Sabattié <t.sabattie@student.isartdigital.com>
2016-05-17 01:30:59 +0200

    Split ResizeComponent to 2 handlers
    ScaleHandler and AlignHandler

--- src/template/utils/game/AlignHandler.hx
+++ src/template/utils/game/AlignHandler.hx
@@ -1,21 +1,15 @@
 package template.utils.game;
-
 import openfl.display.DisplayObject;
 import openfl.display.DisplayObjectContainer;
 import openfl.events.Event;
 import openfl.geom.Point;
-import openfl.geom.Rectangle;
 import template.utils.game.AlignMode;
-import template.utils.game.AlignOrigin;
-import template.utils.game.ScaleMode;
 
 /**
- * Manage behaviour of display object on resize.
- * Manage Alignment and scaling.
- * Note : if you change transform (position or scale) of parents, use forceUpdate method 
+ * ...
  * @author Théo Sabattié
  */
-class ResizeComponent
+class AlignHandler extends HandlerOnResize
 {
 	/**
 	 * Alignment style
@@ -33,17 +27,6 @@ class ResizeComponent
 	public var alignOrigin(default, set):AlignOrigin = AlignOrigin.FROM_STAGE;
 	
 	/**
-	 * Scale mode on resize
-	 * ScaleMode.KEEP_ASPECT conserve aspect even if parents have different scales
-	 * ScaleMode.NO_SCALE    do not scale 
-	 * ScaleMode.SHOW_ALL    use safeZone for ratio
-	 * ScaleMode.FIT_WIDTH 	 fit all width
-	 * ScaleMode.FIT_HEIGHT  fit all height
-	 * ScaleMode.FIT_ALL 	 fit all screen
-	 */
-	public var scaleMode(default, set):ScaleMode = ScaleMode.SHOW_ALL;
-	
-	/**
 	 * Offset x added after alignment
 	 */
 	public var offsetX(get, set):Float;
@@ -53,35 +36,14 @@ class ResizeComponent
 	 */
 	public var offsetY(get, set):Float;
 	
-	/**
-	 * Determines if safe zone is used for alignment
-	 */
-	public var useSafeZone(default, set):Bool = false;
-	
-	private var safeZone:Rectangle = new Rectangle(0, 0, 2048, 1366);
 	private var offset:Point       = new Point(0, 0);
-	private var target:DisplayObject;
 	
 	/**
-	 * @param	target : DisplayOject which will be transform
+	 * @param	target : DisplayOject which will be align
 	 */
 	public function new(target:DisplayObject) 
 	{
-		this.target = target;
-		
-		target.addEventListener(Event.ADDED_TO_STAGE, onAddToStage);
-		target.addEventListener(Event.REMOVED_FROM_STAGE, onRemoveFromStage);
-		
-		if (target.stage != null) {
-			onAddToStage();
-		}
-	}
-	
-	/**
-	 * Force update when parents are transformed
-	 */
-	public function forceUpdate():Void {
-		onResize();
+		super(target);
 	}
 	
 	/**
@@ -111,117 +73,11 @@ class ResizeComponent
 		}
 	}
 	
-	/**
-	 * Set the size of safeZone
-	 * @param	safeZone
-	 */
-	public function setSafeZone(safeZone:Rectangle):Void {
-		this.safeZone = safeZone;
-		
-		if (useSafeZone || scaleMode == ScaleMode.SHOW_ALL) {
-			resizeIfOnStage();
-		}
-	}
-	
-	/**
-	 * Destroy component (component removes all its listeners)
-	 */
-	public function destroy():Void {
-		removeResizeListener();
-		target.removeEventListener(Event.ADDED_TO_STAGE, onAddToStage);
-		target.removeEventListener(Event.REMOVED_FROM_STAGE, onRemoveFromStage);
-	}
-	
-	private function addResizeListener():Void 
+	override function onResize(?event:Event = null):Void 
 	{
-		target.stage.addEventListener(Event.RESIZE, onResize);
-	}
-	
-	private function removeResizeListener():Void {
-		target.stage.removeEventListener(Event.RESIZE, onResize);	
-	}
-	
-	private function onAddToStage(?event:Event=null):Void {
-		addResizeListener();
-		onResize();
-	}
-	
-	private function onRemoveFromStage(event:Event):Void {
-		removeResizeListener();
-	}
-	
-	private function onResize(?event:Event = null):Void {
-		if (scaleMode != ScaleMode.NO_SCALE) {
-			target.scaleX = target.scaleY = 1;
-			
-			if (scaleMode == ScaleMode.KEEP_ASPECT) {
-				scaleToKeepAspect();
-			} else if (scaleMode == ScaleMode.SHOW_ALL) {
-				scaleToShowAll();
-			} else {
-				var ratioStageToSafeZone:Float  = getRatioStageToSafeZone();
-				var targetBounds:Rectangle      = target.getBounds(target.stage);
-				
-				if (scaleMode == ScaleMode.FIT_WIDTH) {
-					scaleToFitWidth(ratioStageToSafeZone, targetBounds);
-				} else if (scaleMode == ScaleMode.FIT_ALL) {
-					scaleToFitAll(ratioStageToSafeZone, targetBounds);
-				} else if (scaleMode == ScaleMode.FIT_HEIGHT) {
-					scaleToFitHeight(ratioStageToSafeZone, targetBounds);
-				}
-			} 
-		}
-			
 		if (alignModeOnResize != AlignMode.NO_ALIGN) {
 			setAlignPos(alignModeOnResize, useSafeZone, alignOrigin, offset);
 		}
-	}
-	
-	private function scaleToFitWidth(ratioStageToSafeZone:Float, targetBounds:Rectangle):Void {
-		if (useSafeZone) {
-			var lWidth:Float = safeZone.width * ratioStageToSafeZone;
-			target.scaleX 	 = lWidth / targetBounds.width;
-		} else {
-			target.scaleX = target.stage.stageWidth / targetBounds.width;
-		}
-	}
-	
-	private function scaleToFitHeight(ratioStageToSafeZone:Float, targetBounds:Rectangle):Void {
-		if (useSafeZone) {
-			var lHeight:Float = safeZone.height * ratioStageToSafeZone;
-			target.scaleY 	  = lHeight / targetBounds.height;
-		} else {
-			target.scaleY = target.stage.stageHeight / targetBounds.height;
-		}
-	}
-	
-	private function scaleToFitAll(ratioStageToSafeZone:Float, targetBounds:Rectangle):Void {
-		scaleToFitHeight(ratioStageToSafeZone, targetBounds);
-		scaleToFitWidth(ratioStageToSafeZone, targetBounds);
-	}
-	
-	private function scaleToShowAll():Void {
-		var lRatio:Float = getRatioStageToSafeZone();
-		target.scaleX    = lRatio / getTargetWorldScaleX();
-		target.scaleY    = lRatio / getTargetWorldScaleY();
-	}
-	
-	private function scaleToKeepAspect():Void 
-	{
-		target.scaleX = 1 / getTargetWorldScaleX();
-		target.scaleY = 1 / getTargetWorldScaleY();
-	}
-	
-	private function getRatioStageToSafeZone():Float {
-		return Math.round(10000 * Math.min(target.stage.stageWidth / safeZone.width, target.stage.stageHeight / safeZone.height)) / 10000;
-	}
-	
-	private function getTargetWorldScaleX():Float {
-		return target.getBounds(target.stage).width / target.width;
-	}
-	
-	private function getTargetWorldScaleY():Float {
-		return target.getBounds(target.stage).height / target.height;
 	}
 	
 	private function alignPosFromParent(alignMode:AlignMode, useSafeZone:Bool, offset:Point):Void 
@@ -293,11 +149,6 @@ class ResizeComponent
 		target.y = basePos.y;
 	}
 	
-	private function throwExceptionNotOnStage():Void 
-	{
-		throw "Comp :: Child is not added on stage, you can not set align position on this context";
-	}
-	
 	private function updateSafeZonePosition():Void 
 	{
 		var lRatio:Float  = getRatioStageToSafeZone();
@@ -347,7 +198,7 @@ class ResizeComponent
 		var isChanged = safeZoneAlignMode != alignMode;
 		safeZoneAlignMode = alignMode;
 		
-		if (isChanged && (useSafeZone || scaleMode == ScaleMode.SHOW_ALL)) {
+		if (isChanged && useSafeZone) {
 			resizeIfOnStage();
 		}
 		
@@ -378,34 +229,6 @@ class ResizeComponent
 		}
 		
 		return alignModeOnResize;
-	}
-	
-	private function set_scaleMode(scaleMode:ScaleMode):ScaleMode {
-		var isChanged:Bool = scaleMode != this.scaleMode;
-		this.scaleMode = scaleMode;
-		
-		if (isChanged) {
-			resizeIfOnStage();
-		}
-		
-		return this.scaleMode;
-	}
-	
-	private function set_useSafeZone(useSafeZone:Bool):Bool {
-		var isChanged:Bool = useSafeZone != this.useSafeZone;
-		this.useSafeZone   = useSafeZone;
-		
-		if (isChanged) {
-			resizeIfOnStage();
-		}
-		
-		return this.useSafeZone;
-	}
-	
-	private function resizeIfOnStage():Void {
-		if (target.stage != null) {
-			onResize();
-		}
 	}
 	
 	private function set_offsetX(x:Float):Float {
