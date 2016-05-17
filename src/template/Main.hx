c5fca6033855cc6e4a6c802106c9f63cd07dafe4
Osilos <f.marianacci@tudent.isartdigital.com>
2016-05-17 23:33:29 +0200

    add some condition to multi platiform compatibility

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -21,17 +21,21 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-//
-		//Debug.initDefaultContainer(this); //todo : create debug container
-		//addEventListener(Event.ENTER_FRAME, Game.gameloop);
-		//
-		//Metadatas.load();
-		//Game.start();
-//
-		//// todo : move to Game.hx when we have GameStage
-		//#if showdebuginfo
-			//var debugInfo:DebugInfo = new DebugInfo();
-			//addChild(debugInfo);
-		//#end
+
+		Debug.initDefaultContainer(this); //todo : create debug container
+		addEventListener(Event.ENTER_FRAME, Game.gameloop);
+		
+		#if !html5 
+			Localization.init();
+		#end
+		
+		Metadatas.load();
+		Game.start();
+
+		// todo : move to Game.hx when we have GameStage
+		#if showdebuginfo
+			var debugInfo:DebugInfo = new DebugInfo();
+			addChild(debugInfo);
+		#end
 	}
 }
