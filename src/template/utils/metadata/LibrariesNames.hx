12197ebb4ea5addb603be0f91df1ea5407f6d8ef
Osilos <f.marianacci@tudent.isartdigital.com>
2016-05-17 23:51:10 +0200

    add some .fla exemple

---
+++ src/template/utils/metadata/LibrariesNames.hx
@@ -0,0 +1,12 @@
+package template.utils.metadata;
+
+/**
+ * ...
+ * @author Flavien
+ */
+class LibrariesNames
+{
+	public static inline var LANDSCAPE_UI:String = "portraitUi";
+	public static inline var PORTRAIT_UI:String = "landscapeUi";
+	
+}
