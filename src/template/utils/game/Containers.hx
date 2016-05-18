6be879f2592d5fdf4a6fb6396ad138ac41fc03c1
dorian <milliere.dorian@openclassrooms.com>
2016-05-18 19:57:34 +0200

    container & method

--- src/template/utils/game/Containers.hx
+++ src/template/utils/game/Containers.hx
@@ -8,6 +8,7 @@ class Containers {
 	public static var game:Sprite;
 	public static var hud:Sprite;
 	public static var menu:Sprite;
+	public static var popin:Sprite;
 	public static var debug:Sprite;
 
 	public static function createContainers():Void {
@@ -15,12 +16,22 @@ class Containers {
 		game = new Sprite();
 		hud = new Sprite();
 		menu = new Sprite();
+		popin = new Sprite();
 		debug = new Sprite();
 
 		Lib.current.stage.addChild(main);
 		main.addChild(game);
 		main.addChild(hud);
 		main.addChild(menu);
+		main.addChild(popin);
 		main.addChild(debug);
+	}
+
+	public static function show(container:Sprite):Void {
+		container.visible = true;
+	}
+
+	public static function hide(container:Sprite):Void {
+		container.visible = false;
 	}
 }
