f02de60d7532665d890b54c787fc0c72fda58734
Osilos <f.marianacci@tudent.isartdigital.com>
2016-05-18 17:37:52 +0200

    Singleton for Main

--- src/template/game/Game.hx
+++ src/template/game/Game.hx
@@ -1,4 +1,6 @@
 package template.game;
+import template.utils.game.GameObject;
+import template.utils.metadata.LibrariesNames;
 
 class Game {
 
@@ -9,6 +11,11 @@ class Game {
 		#if showdebuginfo
 
 		#end
+		
+		var go:GameObject = new GameObject(LibrariesNames.PORTRAIT_UI, "gameObjectTest");
+		go.x = 200;
+		go.y = 200;
+		Main.getInstance().stage.addChild(go);
 	}
 
 	public static function gameloop(event:Dynamic):Void {
