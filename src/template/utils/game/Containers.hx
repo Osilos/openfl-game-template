96963431001c96613db85e4ed64a9d1756c14457
dorian <milliere.dorian@openclassrooms.com>
2016-05-18 19:45:46 +0200

    hud move

--- src/template/utils/game/Containers.hx
+++ src/template/utils/game/Containers.hx
@@ -5,22 +5,22 @@ import openfl.Lib;
 
 class Containers {
 	public static var main:Sprite;
-	public static var menu:Sprite;
 	public static var game:Sprite;
 	public static var hud:Sprite;
+	public static var menu:Sprite;
 	public static var debug:Sprite;
 
 	public static function createContainers():Void {
 		main = new Sprite();
-		menu = new Sprite();
 		game = new Sprite();
 		hud = new Sprite();
+		menu = new Sprite();
 		debug = new Sprite();
 
 		Lib.current.stage.addChild(main);
-		main.addChild(menu);
 		main.addChild(game);
 		main.addChild(hud);
+		main.addChild(menu);
 		main.addChild(debug);
 	}
 }
