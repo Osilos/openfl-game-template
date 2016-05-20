8766b07651f8336f39f19a73848e68fe70e6ba22
dorian <milliere.dorian@openclassrooms.com>
2016-05-20 23:13:13 +0200

    debug fix

--- src/template/game/metadata/LibrariesNames.hx
+++ src/template/game/metadata/LibrariesNames.hx
@@ -4,7 +4,8 @@ package template.game.metadata;
  * ...
  * @author Flavien
  */
-class LibrariesNames {public static inline var LANDSCAPE_UI:String = "landscapeUi";
+class LibrariesNames {
+	public static inline var LANDSCAPE_UI:String = "landscapeUi";
 	public static inline var PORTRAIT_UI:String = "portraitUi";
 
 }
