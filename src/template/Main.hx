13ce420b0057c205743ee12472813f6dd29dbc52
dorian <milliere.dorian@openclassrooms.com>
2016-05-31 16:01:05 +0200

    project

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -1,9 +1,8 @@
 package template;
 
-import template.utils.game.GameObject;
-import template.utils.mobile.AppSharing;
 import haxe.Timer;
-import template.utils.mobile.AppRating;
+import extension.admob.GravityMode;
+import extension.admob.AdMob;
 import template.utils.debug.DebugInfo;
 import template.utils.game.Containers;
 import template.utils.Localization;
@@ -32,6 +31,19 @@ class Main extends Sprite {
 
 		Metadatas.load();
 		Game.start();
+
+//		AdMob.enableTestingAds();
+//		AdMob.initIOS('ca-app-pub-4254723393059263/3604002133', 'ca-app-pub-4254723393059263/1164592937', GravityMode.BOTTOM);
+//		AdMob.showBanner();
+//
+//		Timer.delay(function () {
+//			trace('will show !');
+//		}, 4000);
+//
+//		Timer.delay(function () {
+//			AdMob.showInterstitial(0);
+//		}, 5000);
+
 
 		#if showdebuginfo
 		var debugInfo:DebugInfo = new DebugInfo();
