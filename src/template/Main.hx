ae403a28003027252b487fb9a2053b6988fe2c6f
dorian <milliere.dorian@openclassrooms.com>
2016-06-03 08:41:20 +0200

    legacy on android and main remove test

--- src/template/Main.hx
+++ src/template/Main.hx
@@ -1,13 +1,5 @@
 package template;
 
-import String;
-import openfl.geom.Point;
-import hypsystem.net.NetworkInfos;
-import hypsystem.system.Device;
-import template.utils.game.GameObject;
-import template.utils.mobile.AppSharing;
-import haxe.Timer;
-import template.utils.mobile.AppRating;
 import template.utils.debug.DebugInfo;
 import template.utils.game.Containers;
 import template.utils.Localization;
@@ -36,23 +28,6 @@ class Main extends Sprite {
 
 		Metadatas.load();
 		Game.start();
-
-		var test:String = Localization.getText('loading');
-
-		Debug.addDebugTextAt(new Point(150, 150), test);
-
-
-//		Debug.addDebugTextAt(new Point(150, 150),
-//			Device.getLanguageCode() + '\n' +
-//			Device.getName() + '\n'
-//			Device.getSystemVersion() + '\n' +
-//			Device.isTablet() + '\n' +
-//			Device.getUuid() + '\n' +
-//			Device.getScaleFactor() + '\n' +
-//			NetworkInfos.getConnectionType() + '\n' +
-//			NetworkInfos.isConnected() + '\n' +
-//			NetworkInfos.isWifi()
-//		);
 
 		#if showdebuginfo
 		var debugInfo:DebugInfo = new DebugInfo();
