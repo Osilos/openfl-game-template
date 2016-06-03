91d5a6eef75bc8edb8fa0e6999d6c12ef3de2bba
dorian <milliere.dorian@openclassrooms.com>
2016-06-03 09:20:52 +0200

    add debug mode

--- src/template/utils/config/ApplicationDef.hx
+++ src/template/utils/config/ApplicationDef.hx
@@ -2,6 +2,7 @@ package template.utils.config;
 
 typedef ApplicationDef = {
 	var name:String;
+	var debug:Bool;
 	var appleAppId:String;
 	var androidPackageName:String;
 	var shareInformation:ShareInformationDef;
